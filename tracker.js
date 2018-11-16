be3107dbbcf2900af559f9aa539c852b36239162
1imartinez <42749604+1imartinez@users.noreply.github.com>
2018-11-16 13:57:36 -0600

    almost done with tracker.js

--- tracker.js
+++ tracker.js
@@ -5,16 +5,36 @@ function Tracker(){
   let percent = [];
 
   //implement all six functions below
-  function numOfSeeds(list,value){
+  function numOfSeeds(){
     let found = 0;
-    for(let i=0; i<list.lengh; i++){
-      if(list [i] === value){
+    for(let i=0; i<seeds.lengh; i++){
+      if(list [i] !== null){
         found ++;
       }
     }
     return found;
   }
 
+
+
+
+
+  function addSeed(s){
+    seeds.push(s)
+  }
+
+
+
+
+
+  function removeSeed(name){
+    let index= 0;
+    for(let i=0; i<seeds.length; i++){
+      if(list [i] === name){
+
+      }
+    }
+  }
 
 
 }
