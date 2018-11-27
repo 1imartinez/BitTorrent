7ac1bd89f16d303542aee1ad6dfef3cc399b1345
1imartinez <42749604+1imartinez@users.noreply.github.com>
2018-11-27 13:57:05 -0600

    Ask Mr. Flemming for help for reportHealth()

--- tracker.js
+++ tracker.js
@@ -29,12 +29,38 @@ function Tracker(){
 
   function removeSeed(name){
     let index= 0;
-    for(let i=0; i<seeds.length; i++){
-      if(list [i] === name){
+    for(let i = 0; i < seeds.length; i++){
+      if(seeds[i] === s){
+        
+        seeds[i] =null;
 
-      }
+        percent[i] = 0;
+       }
     }
   }
 
 
+
+
+  function reportHealth(list){
+    let sum=0
+    for(let i=0; i < list.length; i++){
+      if(list [i]!=0){
+        sum=sum+list[i];
+      }
+    }
+    return sum/list.length
+  }
+
+
+
+
+  function update(){
+    Math.random(0,1);
+    for(let i=0; i<list.legth; i++)
+  }
 }
+
+
+
+
